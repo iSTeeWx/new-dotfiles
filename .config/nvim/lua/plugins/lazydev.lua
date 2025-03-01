ebf882b9990f1f749bfad9ad88173e9c7b87e326
iSTeeWx <vdsti2006@gmail.com>
2025-03-01 12:31:35 +0100

    [nvim] removed luvit plugin

--- .config/nvim/lua/plugins/lazydev.lua
+++ .config/nvim/lua/plugins/lazydev.lua
@@ -2,11 +2,6 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua",
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
+    opts = {},
   },
 }
