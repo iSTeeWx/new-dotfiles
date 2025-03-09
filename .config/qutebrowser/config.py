6de185d7d9c2b5bc90910c1af81f08434ec5307a
iSTeeWx <vdsti2006@gmail.com>
2025-03-09 23:52:37 +0100

    [qute] forgot to add config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,21 @@
+import catppuccin
+
+config.load_autoconfig(False)
+catppuccin.setup(c, "Mocha", True)
+c.content.site_specific_quirks.enabled = True
+c.colors.webpage.preferred_color_scheme = "dark"
+
+c.fonts.default_size = "14pt"
+c.fonts.default_family = "Source Code Pro"
+c.statusbar.position = "top"
+
+c.tabs.max_width = 400
+c.tabs.title.alignment = "center"
+c.tabs.title.format = "{audio}[{index}]{current_title}"
+c.tabs.indicator.padding = {"bottom": 4, "left": 0, "right": 4, "top": 4}
+
+config.unbind("+")
+config.bind("<ctrl-=>", "zoom-in")
+
+config.unbind("-")
+config.bind("<ctrl-->", "zoom-out")
