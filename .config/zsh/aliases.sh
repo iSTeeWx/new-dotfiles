b7c285c3041c6eb5a68c7086658113dc76b9b588
iSTeeWx <vdsti2006@gmail.com>
2025-03-15 00:54:23 +0100

    added vscodium

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -5,6 +5,7 @@ alias susp='systemctl suspend'
 alias 1440='xrandr --output eDP1 --mode 2560x1440'
 alias nmtui='NEWT_MONO=1 nmtui'
 alias n='nvim '
+alias code="gtk-launch codium-wayland "
 
 alias gs="git status"
 alias ga="git add "
