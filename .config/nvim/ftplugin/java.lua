ce850d316b32add3cb0c33ef74973620662f5cac
iSTeeWx <37043298+iSTeeWx@users.noreply.github.com>
2025-04-28 23:27:36 +0200

    [nvim] jdtls and processing

---
+++ .config/nvim/ftplugin/java.lua
@@ -0,0 +1,15 @@
+local config = {
+  cmd = {'/usr/share/java/jdtls/bin/jdtls'},
+  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+  settings = {
+    java = {
+      project = {
+        referencedLibraries = {
+          '/opt/processing-4.3.4/core/library/core.jar',
+        },
+      }
+    }
+  }
+}
+
+require('jdtls').start_or_attach(config)
--- .zprofile
+++ .zprofile
@@ -4,4 +4,6 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 export PYTHON_HISTORY=$HOME/.local/share/python/history
 export MYSQL_HISTFILE=$HOME/.local/share/mariadb/history
 
+export PATH=/opt/processing-4.3.4/:$PATH
+
 export EDITOR=nvim
