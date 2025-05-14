11ad8582815bfd01d1cd2c48a0b265751cbbe357
iSTeeWx <37043298+iSTeeWx@users.noreply.github.com>
2025-05-15 00:40:10 +0200

    [nvim] removed mini tabline highlights

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -20,13 +20,6 @@ return {
     },
   },
   init = function()
-    if true then
-      print("a")
-    end
     vim.cmd.colorscheme "catppuccin-mocha"
-    vim.api.nvim_set_hl(0, 'MiniTablineCurrent', { bold = true, italic = true, bg = '#89B4FA', fg = '#121212' })
-    vim.api.nvim_set_hl(0, 'MiniTablineModifiedCurrent', { bold = true, italic = true, bg = '#89B4FA', fg = '#121212' })
-    vim.api.nvim_set_hl(0, 'MiniTablineModifiedHidden', { bold = true, italic = true, bg = '#121212' })
-    vim.api.nvim_set_hl(0, 'MiniTablineModifiedVisible', { bold = true, italic = true, bg = '#121212' })
   end,
 }
