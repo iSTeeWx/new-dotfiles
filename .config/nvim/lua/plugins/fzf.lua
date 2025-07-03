3b3410c33be34c882b2e2c1b93945b9d619d6ae2
iSTeeWx <37043298+iSTeeWx@users.noreply.github.com>
2025-07-03 23:49:25 +0200

    [nvim] added fzf for some lsp stuff

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,17 @@
 return {
   "ibhagwan/fzf-lua",
-  opts = {},
+  opts = {
+    files = {
+      no_ignore = true,
+      fd_opts = [[--color=never --hidden --type f --type l --exclude .git -I]],
+    },
+  },
   keys = {
-    { "<leader><leader>", "<esc><cmd>FzfLua oldfiles<cr>", desc = "Pick in recent files" },
+    { "<leader><leader>", "<esc><cmd>FzfLua oldfiles<cr>",                                                                         desc = "Pick in recent files" },
+    { "<leader>o",        "<esc><cmd>FzfLua files<cr>",                                                                            desc = "Pick all files" },
+    { "grr",              "<esc><cmd>FzfLua lsp_references<cr>",                                                                   desc = "References" },
+    { "gra",              "<esc><cmd>FzfLua lsp_code_actions winopts.row=1 previewer=none winopts.width=1 winopts.height=0.2<cr>", desc = "Code Actions" },
+    { "grd",              "<esc><cmd>FzfLua diagnostics_document<cr>",                                                             desc = "Diagnostics" },
+
   }
 }
